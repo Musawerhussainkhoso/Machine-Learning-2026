2a41bfce5b3591ad375100935c03f7c14d8851ed
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-06-08 07:46:44 +0500

    Basic EDA steps

--- Proj01/ML_01.py
+++ Proj01/ML_01.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
 
 
-
+warnings.filterwarnings('ignore')
+data = pd.read_csv(r'D:\ML\Proj01\insurance.csv')
+df = data.copy()
+#print(data.head())
+print(np.__version__)
+#EDA
+print(df.shape)
+df.info()
+print(df.head())
+print(df.tail())
+print(df.describe()) 
+print(df.isnull().sum()) 
