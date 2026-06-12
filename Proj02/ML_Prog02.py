f84af89115b6afaecac2e60d9ad164a72de2fd6d
MusawerhussainKhoso <musawerhussain10@gmail.com>
2026-06-12 09:29:01 +0500

    EDA DONE

--- Proj02/ML_Prog02.py
+++ Proj02/ML_Prog02.py
@@ -10,3 +10,9 @@ df = pd.read_csv(r'D:\ML\Proj02\heart.csv')
 df_copy = df.copy()
 
 print(df_copy.head())
+#EDA 
+print(df_copy.columns)
+print(df_copy.shape)
+print(df_copy.info())
+print(df_copy.describe())
+print(df_copy.isnull().sum())
